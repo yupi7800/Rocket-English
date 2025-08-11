2c626dd414dd33b4c362c28a556c2e39272953c2
yupi7800 <yupi7800@gmail.com>
2025-08-11 19:21:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-function checkPassword(){
-    const input = document.getElementById('password').value;
-    if(input === 'rocket123'){
-        document.getElementById('lock-screen').style.display = 'none';
-        document.getElementById('main-app').style.display = 'block';
-    } else {
-        document.getElementById('error').textContent = 'Incorrect password';
-    }
-}
+
